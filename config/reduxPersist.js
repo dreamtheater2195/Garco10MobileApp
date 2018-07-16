665d9563fe1abebaeb205860c0af1bc44e81187e
Quan Minh Pham <dreamtheater2195@gmail.com>
2018-07-16 21:45:18 +0700

    fix typo in store config

--- config/reduxPersist.js
+++ config/reduxPersist.js
@@ -1,16 +1,11 @@
 import { AsyncStorage } from 'react-native';
-import { createWhitelistFilter, createBlacklistFilter } from 'redux-persist-transform-filter';
 const REDUX_PERSIST = {
     active: true,
-    reducerVersion: '1.2',
+    reducerVersion: '1.4',
     storeConfig: {
         key: 'root',
         storage: AsyncStorage,
-        transform: [
-            createBlacklistFilter('auth', ['fetching', 'error']),
-            createBlacklistFilter('garco10', ['fetching', 'error']),
-            createBlacklistFilter('network', ['isConnected', 'syncing'])
-        ]
+        blacklist: ['rehydrated']
     }
 }
 
