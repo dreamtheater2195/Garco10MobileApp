19d1bc1acaa20a4dc8a5aafbe8f13b8ed7561d96
Quan Minh Pham <dreamtheater2195@gmail.com>
2018-07-25 15:06:49 +0700

    change HTTP method on an api

--- config/reduxPersist.js
+++ config/reduxPersist.js
@@ -1,7 +1,7 @@
 import { AsyncStorage } from 'react-native';
 const REDUX_PERSIST = {
     active: true,
-    reducerVersion: '1.4',
+    reducerVersion: '1.6',
     storeConfig: {
         key: 'root',
         storage: AsyncStorage,
--- services/api.js
+++ services/api.js
@@ -7,7 +7,7 @@ export function requestUpdateItemQuantity(currentRaChuyen) {
     const source = CancelToken.source();
 
     const requestConfig = {
-        method: 'put',
+        method: 'post',
         url: url,
         timeout: 2000,
         headers: {
@@ -29,7 +29,10 @@ export function requestUpdateItemQuantity(currentRaChuyen) {
 
 export function requestFetchData(ID_LoSanXuat, ID_DonVi) {
     const url = `${API_BASE_URL}/LoSanXuats?ID_LoSanXuat=${ID_LoSanXuat}&ID_DonVi=${ID_DonVi}`;
-    return axios.get(url).then(response => response.data).catch(err => { throw err });
+    return axios.get(url).then(response => {
+        console.log(response.data);
+        return response.data;
+    }).catch(err => { throw err });
 }
 
 export function requestLogin(username, password) {
